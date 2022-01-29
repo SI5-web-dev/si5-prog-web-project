035d3462cb466447c96637649b61ca535a5c6ede
Ossama <ossama1998@windowslive.com>
2022-01-29 18:09:41 +0100

    Création Home page + 2 routes dessus #3
    @Ossama98 @iBananos

--- frontend/acence/src/App.js
+++ frontend/acence/src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+      <Routes>
+        <Route path="/" element={<Home/>}>
+        </Route>
+        <Route path="/home" element={<Home/>}>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
