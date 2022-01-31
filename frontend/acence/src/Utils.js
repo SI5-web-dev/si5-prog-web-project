9cfdeb43ca9b48baf4ac2d320a182a81b4cf5d13
iBananos <ralph.el-chalfoun@etu.unice.fr>
2022-01-31 14:00:26 +0100

    classe utils pour les requetes au serveur

---
+++ frontend/acence/src/Utils.js
@@ -0,0 +1,34 @@
+var pathUrl =  window.location
+let hostname = "";
+
+if (pathUrl.origin === "http://localhost:3000"){
+    hostname =  "http://localhost:4000";
+}
+class Utils {
+    
+
+    /**
+     * @param methode 'POST' 'GET'
+     * @param url ex: '/user/login'
+     * @param data ex: {'password':'gfdgfqhq', 'mail':'teest@user.com'}
+     */
+    static async sendRequestWithToken(methode , url , data , callback) {
+
+        var xhr = new XMLHttpRequest()
+        xhr.open(methode, hostname+url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+      
+        xhr.addEventListener('readystatechange', function(e) {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                callback(this.response);
+            }
+            else if (xhr.readyState === 4 && xhr.status !== 200) {
+                JSON.parse(this.response);
+            }
+        });
+        xhr.send(data);
+    }
+}
+
+
+export default Utils;
