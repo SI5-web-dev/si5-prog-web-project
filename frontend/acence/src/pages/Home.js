0097c243b9c26eab6a74ebacc34280d953605187
iBananos <ralph.el-chalfoun@etu.unice.fr>
2022-01-31 16:25:46 +0100

    fonction pour creer les checkboxs dynamiquement

--- frontend/acence/src/pages/Home.js
+++ frontend/acence/src/pages/Home.js
@@ -1,43 +1,88 @@
 
 import Navigation from "../components/Navigation";
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import {Button , ButtonGroup ,ToggleButton, Form} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 function Home(){
+
+    const [checked, setChecked] = useState(false);
+    const [radioValue, setRadioValue] = useState('1');
+
+    const radiosEssence = [
+        { name: 'Gazole', value: 'Gazole' },
+        { name: 'SP95-E10', value: 'SP95-E10' },
+        { name: 'SP98', value: 'SP98' },
+        { name: 'SP95', value: 'SP95' },
+        { name: 'GPLc', value: 'GPLc' },
+        { name: 'E85', value: 'E85' },
+      ];
+
+
+
+
+
+    function createSettings(){
+        //tranche de prix
+        let prix = document.getElementById('prix')
+        prix.min = 0;
+        prix.max = 20;
+        prix.value =10;
+        let nbService=[];
+        //services
+        let services = document.getElementById('services');
+        for(let i = 0 ; i<nbService.size();i++){
+            let service = nbService[i];
+            let checkbox = document.createElement('input');
+            let label = document.createElement('label');
+            checkbox.type = "checkbox" ;
+            checkbox.id = service;
+            checkbox.name = service;
+            label.htmlFor= service;
+            label.innerHTML = service;
+            services.appendChild(checkbox)
+            services.appendChild(label)
+        }
+    }
+        
+
     return (
         <div className="Home">
             <Navigation />
             <h1>Trouver une station essence</h1>
-            <Button variant="primary">Primary</Button>
             <FontAwesomeIcon icon={faCoffee} />
+            <br />
             <input placeholder="Dans quelle ville voulez-vous trouver votre station essence ?"/>
+        
+            <br />
             <div>
-                <input type="radio" id="Gazole" name="essence" value="Gazole" checked />
-                <label for="Gazole">Gazole</label>
-            </div>
-            <div>
-                <input type="radio" id="SP95-E10" name="essence" value="SP95-E10" checked />
-                <label for="SP95-E10">SP95-E10</label>
-            </div>
-            <div>
-                <input type="radio" id="SP98" name="essence" value="SP98" checked />
-                <label for="SP98">SP98</label>
-            </div>
-            <div>
-                <input type="radio" id="SP95" name="essence" value="SP95" checked />
-                <label for="SP95">SP95</label>
-            </div>
-            <div>
-                <input type="radio" id="GPLc" name="essence" value="GPLc" checked />
-                <label for="GPLc">GPLc</label>
-            </div>
-            <div>
-                <input type="radio" id="E85" name="essence" value="E85" checked />
-                <label for="E85">E85</label>
-            </div>
-            <button>Rechercher la plus proche</button>
-            <button>Rechercher la moins chère</button>
-            
+                {radiosEssence.map((radio, idx) => (
+                <ToggleButton
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    className="m-2"
+                    size="lg"
+                    variant={'secondary' }
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => setRadioValue(e.currentTarget.value)}>   
+                    {radio.name}
+                </ToggleButton>
+                ))}
+                </div>
+                <br />
+                <Button variant="primary" className="m-2" onClick={createSettings}>Rechercher la plus proche</Button>{}
+                <Button variant="primary" className="m-2" onClick={createSettings}>Rechercher la moins chère</Button>{}
+                <div id="researchSettings">
+                    <input type="checkbox" id="ouverte" name="ouverte" defaultChecked />
+                    <label htmlFor="ouverte">Ouverte</label>
+                    <input type='range' id="prix" min="0" max="10"></input>
+                    <div id="services">
+
+                    </div>
+                </div>
         </div>
 
     )
