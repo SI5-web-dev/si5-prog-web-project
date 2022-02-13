66fb6a64964a3d9d7e05a6264335b4a55a1997df
Ossama <ossama1998@windowslive.com>
2022-02-14 00:20:24 +0100

    xml to json

--- backend/src/xmLToJSON.ts
+++ backend/src/xmLToJSON.ts
@@ -4,20 +4,40 @@ import * as util from "util";
 
 let parser = new xml2js.Parser();
 
-function convertXMLToJSON(xmlFile: string, jsonFile: string) {
-    fs.readFile(xmlFile, function (err, data) {
-        parser.parseString(data, function (err: any, result: any) {
-            if (err) {
-                throw err;
-            }
-            console.log(util.inspect(result, false, null))
-            //JSON.stringify(result)
-            //console.log(result);
-            console.log('Done');
-            let writeStream = fs.createWriteStream(jsonFile);
-            writeStream.write(util.inspect(result, false, null));
+function convertXMLToJSON(xmlFile: string) {
+    return new Promise((resolve) => {
+        fs.readFile(xmlFile,"utf8", function (err, data) {
+            parser.parseString(data, function (err: any, result: any) {
+                if (err) {
+                    throw err;
+                }
+                console.log('Done');
+
+                let json = util.inspect(result, false, null);
+                console.log(`Type : ${typeof json}`);
+
+                json = json.slice(26);
+                json = json.slice(0 , json.length -6 );
+                json = JSON.parse(JSON.stringify(json));
+
+                //JSON.stringify(result);
+                //fs.createWriteStream("./ossaZ.json").write(json);
+                //console.log(json);
+
+                resolve(json);
+            });
         });
-    });
+    })
 }
 
 export default convertXMLToJSON;
+
+
+/*let writeStream = fs.createWriteStream(jsonFile);
+            writeStream.write(util.inspect(result, false, null));
+            writeStream.on('finish', function () {
+                console.log('file has been written');
+            });
+            writeStream.on('end', function () {
+                console.log('Enddddddd');
+            });*/
