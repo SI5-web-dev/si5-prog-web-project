4350f0da2acd0105f0218971ff8ac6c086dd3675
Ossama <ossama1998@windowslive.com>
2022-02-14 17:08:31 +0100

    Création du tableau dynamiquement (pas encore fini)

--- frontend/acence/src/components/ListEssence.js
+++ frontend/acence/src/components/ListEssence.js
@@ -1,116 +1,43 @@
-import {Table, Badge, Button} from "react-bootstrap";
-import React, {useState} from "react";
+import { Table, Badge, Button } from "react-bootstrap";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown, faStar } from '@fortawesome/free-solid-svg-icons'
-import {useRef} from "react";
+import { useRef } from "react";
 import CanvasInfosEssence from "./CanvasInfosEssence";
 
-const ListEssence = () => {
+const ListEssence = (props) => {
 
     const childRef = useRef();
 
     const [nameStation, setNameStation] = useState(0);
 
-    function displayInfosStation(station){
+    function displayInfosStation(station) {
         setNameStation(station);
         childRef.current.getAlert();
     }
 
+    function CreateList() {
+        return (
+            props.list.map(station => {
+                return (
+                    <tr>
+                        <td>{station[2]}</td>
+                        <td>{station[3]}</td>
+                        <td>{station[4]}</td>
+                        <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
+                    </tr>)
+            })
+        )
+    }
+
     return (
         <div className="listEssence">
-        <Table responsive="sm">
-        <tbody>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="danger">
-                Fermée
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="success">
-                Ouvert
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="danger">
-                Fermée
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="danger">
-                Fermée
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="danger">
-                Fermée
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="danger">
-                Fermée
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="danger">
-                Fermée
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        <tr>
-            <td>Station de Carrefour - Antibes - adresse</td>
-            <td>SP95-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td>SP98-1.702 <FontAwesomeIcon icon={faChevronDown} color={"green"}/></td>
-            <td>Diesel-1.702 <FontAwesomeIcon icon={faChevronUp} color={"red"}/></td>
-            <td><FontAwesomeIcon icon={faStar}/></td>
-            <td><Badge pill bg="danger">
-                Fermée
-            </Badge></td>
-            <td><Button variant="secondary" size="sm" onClick={()=>{displayInfosStation("Station de Carrefour")}}>Plus d'infos</Button></td>
-        </tr>
-        </tbody>
-        </Table>
-            <CanvasInfosEssence ref={childRef} nameStation={nameStation}/>
+            <Table responsive="sm">
+                <tbody>
+                    <CreateList />
+                </tbody>
+            </Table>
+            <CanvasInfosEssence ref={childRef} nameStation={nameStation} />
         </div>
     );
 };
